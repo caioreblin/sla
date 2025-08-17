968b4d0ae68e43c5f07cb60bd2afa226b0d661a0
Caio Reblin <caioreblin07@gmail.com>
2025-08-17 05:04:07 -0300

    alteracoes

--- script.js
+++ script.js
@@ -1,44 +1,46 @@
 const botao = document.getElementById("botao");
 const telaInicial = document.querySelector(".tela-inicial");
-const cartao = document.querySelector(".cartao");
+const cartao1 = document.querySelector(".cartao1");
+const cartao2 = document.querySelector(".cartao2");
 
-const acessos = localStorage.getItem('acessos') || 0;
-const novoValor = Number(acessos) + 1;
-localStorage.setItem('acessos', novoValor);
+// const acessos = localStorage.getItem('acessos') || 0;
+// const novoValor = Number(acessos) + 1;
+// localStorage.setItem('acessos', novoValor);
 
-const mensagensEspeciais = [
-    "Já é a {n}ª vez que você entra aqui, tô de olho 👀",
-    "Vou ter que tirar isso do ar pra você não ficar entrando tanto 😅",
-    "Você tá viciada nessa página, né?",
-    "Isso tá virando rotina já hein, rsrs"
-];
+// const mensagensEspeciais = [
+//     "Já é a {n}ª vez que você entra aqui, tô de olho 👀",
+//     "Vou ter que tirar isso do ar pra você não ficar entrando tanto 😅",
+//     "Você tá viciada nessa página, né?",
+//     "Isso tá virando rotina já hein, rsrs"
+// ];
 
-if (novoValor >= 3) {
-    const telaInicial = document.querySelector('.tela-inicial');
-    const linha1 = document.querySelector('.linha1');
-    const linha2 = document.querySelector('.linha2');
-    const toque = document.querySelector('#botao');
+// if (novoValor >= 3) {
+//     const telaInicial = document.querySelector('.tela-inicial');
+//     const linha1 = document.querySelector('.linha1');
+//     const linha2 = document.querySelector('.linha2');
+//     const toque = document.querySelector('#botao');
 
-    let msg;
-    if (novoValor === 3) {
-        msg = "Terceira vez aqui? Pode pedir música no Fantástico.";
-    } else {
-        msg = mensagensEspeciais[Math.floor(Math.random() * mensagensEspeciais.length)]
-            .replace('{n}', novoValor);
-    }
+//     let msg;
+//     if (novoValor === 3) {
+//         msg = "Terceira vez aqui? Pode pedir música no Fantástico.";
+//     } else {
+//         msg = mensagensEspeciais[Math.floor(Math.random() * mensagensEspeciais.length)]
+//             .replace('{n}', novoValor);
+//     }
 
-    linha1.classList.remove('linha1');
-    linha2.classList.remove('linha2');
-    linha1.style.borderRight = 'none';
-    linha2.style.borderRight = 'none';
-    linha1.classList.add('msgAleatoria');
+//     linha1.classList.remove('linha1');
+//     linha2.classList.remove('linha2');
+//     linha1.style.borderRight = 'none';
+//     linha2.style.borderRight = 'none';
+//     linha1.classList.add('msgAleatoria');
 
-    linha1.textContent = msg;
-    linha2.textContent = '';
-}
+//     linha1.textContent = msg;
+//     linha2.textContent = '';
+// }
 
 
 botao.addEventListener("click", () => {
     telaInicial.classList.add("hidden");
-    cartao.classList.remove("hidden");
+    cartao1.classList.remove("hidden");
+    cartao2.classList.remove("hidden");
 });
